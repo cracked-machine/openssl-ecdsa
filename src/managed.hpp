b8d85197557af0af9a6e38ac58fdb60faa434d07
cracked-machine <chrisjsutton@gmail.com>
2024-10-02 23:30:29 +0100

    tidied up code and add comments

--- src/managed.hpp
+++ src/managed.hpp
@@ -8,13 +8,14 @@
 #include <memory>
 #include <iostream>
 
+// https://godbolt.org/z/aer7K5fb1
+
 template<class T>
 class OpensslSharedPtr
 {
 public:
-    OpensslSharedPtr(T* _p) {};
-    ~OpensslSharedPtr() {};
-    void checkptr(T* _p) { if (!_p) { std::cout << "nullptr Error" << std::endl; } }
+    OpensslSharedPtr(T* _p) { /* override using explicit specialization */ };
+    void nullptr_handler(T* _p) { if (!_p) { std::cout << "nullptr Error" << std::endl; } }
     T* get() { return m_ptr.get(); };
 private:
     std::shared_ptr<T> m_ptr;
@@ -23,7 +24,8 @@ private:
 // specialise and override constructor that calls pointer checker 
 // and adds specialised deleter for BIO type.
 template<> OpensslSharedPtr<BIO>::OpensslSharedPtr(BIO* _p) { 
-    checkptr(_p);
+    nullptr_handler(_p);
+     // Since shared_ptr sets the deleter during initialization we can use lambdas
     m_ptr = std::shared_ptr<BIO>(_p, [](BIO* _p) 
     { 
         if (_p) BIO_free(_p); 
@@ -34,24 +36,27 @@ template class OpensslSharedPtr<BIO>;
 
 ////////////////////////////////////////////////
 
-struct OpensslDeleter {
-    void operator()(BIO *_p) { BIO_free(_p); }
-};
+// deleters for unique_ptr must already be declared for member template declaration (m_ptr)
+struct OpensslDeleter;
 
 template<class T>
 class OpensslUniquePtr
 {
 public:
-    OpensslUniquePtr(T* _p) {};
-    ~OpensslUniquePtr() {};
-    void checkptr(T* _p) { if (!_p) { std::cout << "invalid nullptr arg" << std::endl; } }
+    OpensslUniquePtr(T* _p) { /* override using explicit specialization */ };
+    void nullptr_handler(T* _p) { if (!_p) { std::cout << "invalid nullptr arg" << std::endl; } }
     T* get() { return m_ptr.get(); }
 private:
-
     std::unique_ptr<T, OpensslDeleter> m_ptr;
 };
 
-template<> OpensslUniquePtr<BIO>::OpensslUniquePtr(BIO* _p) {     
+// deleters for unique_ptr must already be defined for explicit specialization
+struct OpensslDeleter {
+    void operator()(BIO *_p) { BIO_free(_p); }
+};
+
+template<> OpensslUniquePtr<BIO>::OpensslUniquePtr(BIO* _p) {    
+    nullptr_handler(_p); 
     m_ptr = std::unique_ptr<BIO, OpensslDeleter>(_p);
 }; 
 
